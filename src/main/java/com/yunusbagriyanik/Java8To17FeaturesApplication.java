27010fed93aaa28ef1c176ae240bfb166367ad6f
Yunus Bagriyanik <yunusbgrynk@gmail.com>
2023-11-04 17:42:48 +0300

    initial commit

---
+++ src/main/java/com/yunusbagriyanik/Java8To17FeaturesApplication.java
@@ -0,0 +1,13 @@
+package com.yunusbagriyanik;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Java8To17FeaturesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Java8To17FeaturesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yunusbagriyanik/Java8To17FeaturesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yunusbagriyanik;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Java8To17FeaturesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
