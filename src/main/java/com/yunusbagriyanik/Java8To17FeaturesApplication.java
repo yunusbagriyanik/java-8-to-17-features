ddbb2926dd955d5fab2730c2ff1215d4622b33a3
Yunus Bagriyanik <yunusbgrynk@gmail.com>
2023-11-05 01:37:17 +0300

    reformat code

--- src/main/java/com/yunusbagriyanik/Java8To17FeaturesApplication.java
+++ src/main/java/com/yunusbagriyanik/Java8To17FeaturesApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Java8To17FeaturesApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Java8To17FeaturesApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(Java8To17FeaturesApplication.class, args);
+    }
 }
